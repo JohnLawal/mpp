e18804128fea16907818b9fa5f33c1470af7fcb5
John Lawal <jilawal@mum.edu>
2019-06-12 22:57:00 -0500

    Added Day2 prob3

---
+++ lab8/src/day2/prob3/Main.java
@@ -0,0 +1,17 @@
+package day2.prob3;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+		List<String> fruits = Arrays.asList("Apple", "Banana", "Orange", "Cherries", "blums");
+		System.out.println("Using lamdas");
+		fruits.forEach(f -> System.out.println(f));
+		System.out.println("\nUsing method reference");
+		fruits.forEach(System.out::println);
+
+	}
+
+}
