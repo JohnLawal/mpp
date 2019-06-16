25e878d698da7fb97ec7d162db28c20c683a2082
John Lawal <jilawal@mum.edu>
2019-06-16 16:02:24 -0500

    Added Prob2

---
+++ lab9/src/prob2/Main.java
@@ -0,0 +1,23 @@
+package prob2;
+
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Optional<Integer> max = getStream().max(Integer::compareTo);
+		Optional<Integer> min = getStream().min(Integer::compareTo);
+
+		System.out.println("Maximum Integer is: " + max.orElse(null));
+		System.out.println("Minimum Integer is: " + min.orElse(null));
+
+	}
+
+	private static Stream<Integer> getStream() {
+		Stream<Integer> intStream = Arrays.asList(2, 3, 4, 6, 1, 8, 20).stream();
+		return intStream;
+	}
+
+}
