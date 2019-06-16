2620bbb8e22f12f14e1ab011f04891aae517495d
John Lawal <jilawal@mum.edu>
2019-06-16 17:04:23 -0500

    Added prob5

---
+++ lab9/src/prob5/Section.java
@@ -0,0 +1,35 @@
+package prob5;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+//Create a method
+//Stream<String> streamSection(Stream<String> stream, int m, int n) which
+//extracts a substream from the input stream consisting of all elements from position m to
+//position n, inclusive; you must use only Stream operations to do this. You can assume 0 <= m <= n.
+
+public class Section {
+	public static Stream<String> streamSection(Stream<String> stream, int m, int n) {
+		return stream.limit(n).skip(m-1);
+	}
+
+	public static void main(String[] args) {
+// Make three calls for the streamSection() method with different inputs 
+		// use nextStream() method to supply the
+//		Stream input as a first argument in streamSection() method
+		
+		System.out.println("\nFirst Call");
+		streamSection(nextStream(), 1, 5).collect(Collectors.toList()).forEach(System.out::println);
+		System.out.println("\nSecond Call");
+		streamSection(nextStream(), 2, 7).collect(Collectors.toList()).forEach(System.out::println);
+		System.out.println("\nThird Call");
+		streamSection(nextStream(), 5, 9).collect(Collectors.toList()).forEach(System.out::println);
+	
+	}
+
+//support method for the main method -- for testing
+	private static Stream<String> nextStream() {
+		return Arrays.asList("aaa", "bbb", "ccc", "ddd", "eee", "fff", "ggg", "hhh", "iii").stream();
+	}
+}
