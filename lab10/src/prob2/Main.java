e9eb80aadb64b5c1ed4ee8d37b116a75218545ea
John Lawal <jilawal@mum.edu>
2019-06-17 19:45:48 -0500

    Refined prob2

--- lab10/src/prob2/Main.java
+++ lab10/src/prob2/Main.java
@@ -6,12 +6,13 @@ import java.util.List;
 public class Main {
 
 	public static void main(String[] args) {
-		List<Integer> list = Arrays.asList(1, 12, 3, 4, 5);
+		List<Double> list = Arrays.asList(1.1, 1.2, 3.2);
 		System.out.println(secondSmallest(list));
 	}
 
 	@SuppressWarnings("unchecked")
-	public static <T extends Comparable<? super T>> T secondSmallest(List<T> list) {	
+	public static <T extends Comparable<? super T>> T secondSmallest(List<T> list) {
+		//assumes that there will be at least two distinct elements in the list
 		return (T) list.stream().sorted(T::compareTo).distinct().toArray()[1];
 	}
 
